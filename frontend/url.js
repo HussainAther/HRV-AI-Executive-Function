41b44d0c4f9521d4b28d12161daa9334539bd519
Syed Hussain Ather <shussainather@gmail.com>
2025-09-21 22:01:16 -0400

    neato burrito

--- frontend/url.js
+++ frontend/url.js
@@ -1,6 +1,6 @@
 // frontend/url.js
 
-const BASE_URL = "http://localhost:8000"; // adjust if needed for deployment
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const API = {
   PREDICT: `${BASE_URL}/predict`,
