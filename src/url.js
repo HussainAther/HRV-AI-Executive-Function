2a9c393783e3f1a8ca751eefaa3cab2b9f32e68f
Syed Hussain Ather <shussainather@gmail.com>
2025-11-19 17:01:05 -0500

    init

--- src/url.js
+++ src/url.js
@@ -1,3 +1,15 @@
+import config from "./config";
+
+const API = {
+  PREDICT: `${config.API_BASE_URL}/predict`,
+  LOG_HABIT: `${config.API_BASE_URL}/log-habit`,
+  GET_HABITS: `${config.API_BASE_URL}/get-habits`,
+  STREAKS: `${config.API_BASE_URL}/streaks`,
+  LEADERBOARD: `${config.API_BASE_URL}/leaderboard`,
+  PREDICTION_LOG: `${config.API_BASE_URL}/api/prediction-log`,
+};
+
+export default API;
 // src/url.js
 import config from "@/config";
 
